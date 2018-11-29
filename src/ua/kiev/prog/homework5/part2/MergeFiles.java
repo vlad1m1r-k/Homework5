ef364e75f5a6b42e9baf74650769ed74d1f20356
Vladimir <vova.kvz3@gmail.com>
2018-11-29 12:56:55 +0200

    ""

--- src/ua/kiev/prog/homework5/part2/MergeFiles.java
+++ src/ua/kiev/prog/homework5/part2/MergeFiles.java
@@ -1,24 +1,27 @@
 package ua.kiev.prog.homework5.part2;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 
 public class MergeFiles {
     public static void main(String[] args) {
-        if (args.length < 4) System.out.println("Usage: MergeFiles FileName1 FileName2 TargetFileName");
-        Path file1 = Paths.get(args[0]);
-        Path file2 = Paths.get(args[1]);
-        if (Files.exists(file1) && Files.exists(file2)) {
-            try {
-                Path targetFile = Files.copy(file1, Paths.get(args[3]), StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) {
-                e.printStackTrace();
+        if (args.length < 3) System.out.println("Usage: MergeFiles FileName1 FileName2 TargetFileName");
+        else {
+            Path file1 = Paths.get(args[0]);
+            Path file2 = Paths.get(args[1]);
+            if (Files.exists(file1) && Files.exists(file2)) {
+                try {
+                    Path targetFile = Files.copy(file1, Paths.get(args[2]), StandardCopyOption.REPLACE_EXISTING);
+                    Files.write(targetFile, Files.readAllBytes(file2), StandardOpenOption.APPEND);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("File operation error.");
+                }
+            } else {
+                System.out.println("One or more input files not found.");
+                System.out.println(file1.toFile().getAbsolutePath());
+                System.out.println(file2.toFile().getAbsolutePath());
             }
-        } else {
-            System.out.println("Input files not found.");
         }
     }
 }
